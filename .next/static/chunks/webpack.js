085b8eb5c183b923016a070c6dad1931acd05d67
Ravish <ravish@LAPTOP-DHDMJQEM>
2025-09-26 11:46:21 +0530

    api removed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "98bf6e3654969ccf"; }
+/******/ 		__webpack_require__.h = function() { return "48c4295fa799362b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
